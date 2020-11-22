99f2292c018d3c6861e80abba5e7c3b0bd97b78f
Tristan Nguyen <trungupdate@gmail.com>
2020-11-22 10:26:39 +0700

    android-app

--- classifier/trainer.py
+++ classifier/trainer.py
@@ -1,4 +1,7 @@
 # Imports
+import torch.optim as optim
+import torchvision.models.quantization as models
+from torch import nn
 from torchvision import transforms, datasets
 import torchvision
 import torch
@@ -67,7 +70,7 @@ fig, ax = plt.subplots(1, figsize=(10, 10))
 imshow(out, title=[class_names[x] for x in classes], ax=ax)
 
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25, device='cpu'):
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25, device='cuda'):
     """
     Support function for model training.
 
@@ -175,3 +178,54 @@ def visualize_model(model, rows=3, cols=3):
                     model.train(mode=was_training)
                     return
         model.train(mode=was_training)
+
+
+# You will need the number of filters in the `fc` for future use.
+# Here the size of each output sample is set to 2.
+# Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+model_fe = models.resnet18(pretrained=True, progress=True, quantize=True)
+num_ftrs = model_fe.fc.in_features
+
+
+def create_combined_model(model_fe):
+    # Step 1. Isolate the feature extractor.
+    model_fe_features = nn.Sequential(
+        model_fe.quant,  # Quantize the input
+        model_fe.conv1,
+        model_fe.bn1,
+        model_fe.relu,
+        model_fe.maxpool,
+        model_fe.layer1,
+        model_fe.layer2,
+        model_fe.layer3,
+        model_fe.layer4,
+        model_fe.avgpool,
+        model_fe.dequant,  # Dequantize the output
+    )
+
+    # Step 2. Create a new "head"
+    new_head = nn.Sequential(
+        nn.Dropout(p=0.5),
+        nn.Linear(num_ftrs, 2),
+    )
+
+    # Step 3. Combine, and don't forget the quant stubs.
+    new_model = nn.Sequential(
+        model_fe_features,
+        nn.Flatten(1),
+        new_head,
+    )
+    return new_model
+
+
+new_model = create_combined_model(model_fe)
+new_model = new_model.to('cpu')
+
+criterion = nn.CrossEntropyLoss()
+
+# Note that we are only training the head.
+optimizer_ft = optim.SGD(new_model.parameters(), lr=0.01, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = optim.lr_scheduler.StepLR(
+    optimizer_ft, step_size=7, gamma=0.1)
